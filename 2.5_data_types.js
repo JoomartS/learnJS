36bea6812a3584940daf3a60dfcb4cc9450c8185
JoomartS <zhoomartsamiev@gmail.com>
2022-04-07 11:54:38 +0600

    no changes yet

---
+++ 2.5_data_types.js
@@ -0,0 +1,9 @@
+//задача 1
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ? выйдет hello 1
+
+alert( `hello ${"name"}` ); // ? выйдет hello name
+
+alert( `hello ${name}` ); // ? выйдет hello Ilya
+
---
+++ 2.6_prompt.js
@@ -0,0 +1,6 @@
+//Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+
+
+let userName = prompt('what is your name?', '');
+
+alert(`Your name is ${userName}`);
