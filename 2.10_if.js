b7a664836e184d26cb7f96f23de4447f18d7d988
JoomartS <zhoomartsamiev@gmail.com>
2022-04-09 17:17:26 +0600

    if и знак вопроса

---
+++ 2.10_if.js
@@ -0,0 +1,9 @@
+//task 2
+
+let accessAlowed = prompt('Official name of JS', '');
+
+if (accessAlowed == "ECMAScript") {
+        alert( "right")
+} else {
+        alert ("Dont you know ECMASCRIPT?")
+}
---
+++ 2.10_if_3.js
@@ -0,0 +1,17 @@
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+let result;
+
+let a = 6;
+let b = a - (a * 2);
+
+(a + b < 4) ? result = "мало" : result = "много"
+
+
+console.log(result);
